22f6c67a9a78ec1fb4470741e2b4d15f000380b7
abakerPersonal <33030714+abakerPersonal@users.noreply.github.com>
2025-06-29 21:12:44 -0400

    Updater dockerfiles and build

--- build-and-deploy.sh
+++ build-and-deploy.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-
-echo "Building site and docker container"
-nvm use 23
-npm run build-docker
-
 echo "Running Docker Compose"
-docker compose -f ~/homelab/docker-compose.yml up -d resume-site
+docker compose up --build
